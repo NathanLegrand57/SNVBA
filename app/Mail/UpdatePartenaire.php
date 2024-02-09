d86c7dc8c8ae3d829839534479eb2af3668f34c2
Nathan <nathanlgd14@gmail.com>
2024-02-09 16:50:50 +0100

    Ajout fonctionnalité update partenaire + mail lors d'une modification

---
+++ app/Mail/UpdatePartenaire.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Partenaire;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class UpdatePartenaire extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $partenaire;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Partenaire $partenaire)
+    {
+        $this->partenaire = $partenaire;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Mise à jour de vos informations',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.updatepartenaire',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
